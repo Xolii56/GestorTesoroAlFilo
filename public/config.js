89afd4e65e91b69e000459142f7ceb9ecd170dca
Xolii56 <58446246+Xolii56@users.noreply.github.com>
2025-11-12 22:21:37 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // Copia este archivo a config.js y rellena tus datos
 window.SUPABASE_CONFIG = {
-  supabaseUrl: "https://YOUR-PROJECT-ref.supabase.co",
-  supabaseAnonKey: "YOUR_ANON_PUBLIC_KEY",
+  supabaseUrl: "https://juvhyzjjtwfokgnrnejv.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1dmh5empqdHdmb2tnbnJuZWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NzgyMjcsImV4cCI6MjA3ODU1NDIyN30.UupfWq4uGndetyhmEZVHmhdVYNENSf7agKL4FjUF3Hw",
   redirectUrl: "http://localhost:3000" // o tu dominio vercel
 };
