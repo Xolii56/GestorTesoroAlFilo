ca3e0c5671ff0b4f4fee5102ec56fc3e32d49c8f
Xolii56 <58446246+Xolii56@users.noreply.github.com>
2025-11-12 23:17:05 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@
 window.SUPABASE_CONFIG = {
   supabaseUrl: "https://juvhyzjjtwfokgnrnejv.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1dmh5empqdHdmb2tnbnJuZWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NzgyMjcsImV4cCI6MjA3ODU1NDIyN30.UupfWq4uGndetyhmEZVHmhdVYNENSf7agKL4FjUF3Hw",
-  redirectUrl: "https://gestor-tesoro-al-filo.vercel.app/" // o tu dominio vercel
+  redirectUrl: window.location.origin + "app_ui.html"
 };
